7f1f4be6140f86cedcd9080499f7f67166326647
aylin268 <aylin268@ucsb.edu>
2025-07-11 11:00:51 -0700

    Update PauseVec.h

--- pausevec/PauseVec.h
+++ pausevec/PauseVec.h
@@ -1,1 +1,32 @@
+#ifndef PAUSEVEC_H
+#define PAUSEVEC_H
 
+#include <cstddef>
+#include <stdexcept>
+
+
+class PauseVec {
+private:
+  int* data;
+  bool* removed;
+  size_t cap;
+  size_t numLeft;
+  size_t minRemovedInd;
+
+
+public:
+  PauseVec();
+  ~PauseVec();
+
+  size_t capacity() const;
+  size_t count() const;
+
+  int    lookup(size_t index);
+  int    remove(size_t index);
+  void	 remove_val(int x);
+  void   append(int value);
+};
+
+PauseVec* create_pausevec();
+
+#endif
